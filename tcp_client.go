2b9d79be3febb96d8184656107d61f465d9e8b24
Janis Gailis <janis.gailis@uia.no>
2023-04-16 16:38:16 +0000

    port

--- tcp_client.go
+++ tcp_client.go
@@ -4,16 +4,23 @@ import (
 	"net"
 	"log"
 	"os"
+        "github.com/uia-worker/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8200")
+	conn, err := net.Dial("tcp", "127.0.0.1:")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
-	_, err = conn.Write([]byte(os.Args[1]))
+        
+        // Krypter innholdet i os.Args[1]
+        kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+        log.Println(string(kryptertMelding))
+
+//	_, err = conn.Write([]byte(os.Args[1]))
+        _, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
